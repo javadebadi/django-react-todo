4365dc47ea76bdbcea9838e488c2633f9b5ee714
javadebadi <javad.ebadi.1990@gmail.com>
2020-12-06 12:56:48 +0330

    connected backend to frontend successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
-import './App.css';
-
-const list = [
-  {
-    "id": 1,
-    "title": "Read Books",
-    "body": "I have to read 100 pages about django project. In addition, I have to read some philosopical books."
-  },
-  {
-    "id": 2,
-    "title": "Write Proposals",
-    "body": "I have to write a few paragraphs for our proposal. I need to do research and summarize my results."
-  }
-]
+import axios from 'axios';
+import './App.css'
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {list};
+    this.state = {todos:[]};
+  }
+
+  componentDidMount() {
+    this.getTodos();
+  }
+
+  getTodos() {
+    axios.get('http://127.0.0.1:8000/api/').then(
+      res =>this.setState({todos: res.data})
+    ).catch(
+      err => console.log(err))
   }
 
   render () {
     return (
       <div>
-        {this.state.list.map(item => (
+        {this.state.todos.map(item => (
           <div key={item.id}>
             <h1>{ item.title }</h1>
             <p>{ item.body }</p>
