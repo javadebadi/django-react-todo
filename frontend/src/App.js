6a3273956b8c13123d00bb5c60cec9aded9e7c2d
javadebadi <javad.ebadi.1990@gmail.com>
2020-12-06 12:23:57 +0330

    Update: updated react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    "id": 1,
+    "title": "Read Books",
+    "body": "I have to read 100 pages about django project. In addition, I have to read some philosopical books."
+  },
+  {
+    "id": 2,
+    "title": "Write Proposals",
+    "body": "I have to write a few paragraphs for our proposal. I need to do research and summarize my results."
+  }
+]
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {list};
+  }
+
+  render () {
+    return (
+      <div>
+        {this.state.list.map(item => (
+          <div key={item.id}>
+            <h1>{ item.title }</h1>
+            <p>{ item.body }</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
